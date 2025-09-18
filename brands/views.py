10784836a9f421800a004787ed13d2293ffa68d4
kauan <kauansbflu@gmail.com>
2025-09-18 17:35:09 -0300

    feat: update sidebar link to point to brand list and implement brand filtering in view

--- brands/views.py
+++ brands/views.py
@@ -7,3 +7,12 @@ class BrandListView(ListView):
     template_name = "brand_list.html"
     context_object_name = "brands"
     paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.GET.get('name')
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
