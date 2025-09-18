5e77ad64b3d143af3852b1a65e520ba76a4b65df
kauan <kauansbflu@gmail.com>
2025-09-18 19:02:09 -0300

    feat: add brand detail view and template, update brand list links to point to detail view

--- brands/views.py
+++ brands/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 
 from .models import Brand
 from brands import forms
@@ -10,6 +10,7 @@ class BrandListView(ListView):
     template_name = "brand_list.html"
     context_object_name = "brands"
     paginate_by = 10
+    ordering = ['id']
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -19,9 +20,15 @@ class BrandListView(ListView):
             queryset = queryset.filter(name__icontains=name)
 
         return queryset
+
     
 class BrandCreateView(CreateView):
     model = Brand
     template_name = "brand_create.html"
     form_class = forms.BrandForm
     success_url = reverse_lazy('brand_list')
+
+
+class BrandDetailView(DetailView):
+    model = Brand
+    template_name = "brand_detail.html"
