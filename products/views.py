acab0d584e25b494cc73a895a9bcd8a49a7646c1
kauan <kauansbflu@gmail.com>
2025-10-02 18:58:00 -0300

    feat: enhance ProductListView with filtering and context data for categories and brands

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from . import models, forms
+from brands.models import Brand
+from categories.models import Category
 
 
 class ProductListView(ListView):
@@ -8,6 +10,29 @@ class ProductListView(ListView):
     template_name = 'product_list.html'
     context_object_name = 'products'
     paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        title = self.request.GET.get('title')
+        serie_number = self.request.GET.get('serie_number')
+        category = self.request.GET.get('category')
+        brand = self.request.GET.get('brand')
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if serie_number:
+            queryset = queryset.filter(serie_number__icontains=serie_number)
+        if category:
+            queryset = queryset.filter(category_id=category)
+        if brand:
+            queryset = queryset.filter(brand__id=brand)
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = models.Category.objects.all()
+        context['brands'] = models.Brand.objects.all()
+        return context
 
 
 class ProductCreateView(CreateView):
