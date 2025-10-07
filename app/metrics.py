b759c444d1cceccba3505d743ddaebd1af79f4a8
kauan <kauansbflu@gmail.com>
2025-10-07 16:23:56 -0300

    feat: implement product metrics demo display on home page with updated views and templates

---
+++ app/metrics.py
@@ -0,0 +1,12 @@
+
+
+
+def get_product_metrics():
+    # Dummy implementation;
+    return {
+        'total_quantity': 150,
+        'total_cost_price': 5000.00,
+        'total_selling_price': 7500.00,
+        'total_profit': 2500.00,
+    }
+
--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('', views.home, name='home'),
     path('', include('brands.urls')),
     path('', include('categories.urls')),
     path('', include('suppliers.urls')),
