e05741579570881b14875ad757bc1f5bf6576720
kauan <kauansbflu@gmail.com>
2025-10-27 15:15:29 -0300

    feat: include product metrics in home and product list templates

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+
+from app import metrics
 from . import models, forms
 from brands.models import Brand
 from categories.models import Category
@@ -30,8 +32,9 @@ class ProductListView(ListView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['categories'] = models.Category.objects.all()
-        context['brands'] = models.Brand.objects.all()
+        context['product_metrics'] = metrics.get_product_metrics()
+        context['categories'] = Category.objects.all()
+        context['brands'] = Brand.objects.all()
         return context
 
 
