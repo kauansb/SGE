a7c4f694bb92420d0d274e362e7b7f0762eb3c85
kauan <kauansbflu@gmail.com>
2025-10-27 15:26:04 -0300

    feat: include sales metrics in outflow list view and template

--- outflows/views.py
+++ outflows/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView
+
+from app import metrics
 from . import models, forms
 
 
@@ -18,6 +20,12 @@ class OutflowListView(ListView):
             queryset = queryset.filter(product__title__icontains=product)
 
         return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sales_metrics'] = metrics.get_sales_metrics()
+        
+        return context
 
 
 class OutflowCreateView(CreateView):
