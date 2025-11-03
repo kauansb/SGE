dd6ac98aaa9c359fa2d4fe48fe921c7e0b87b58a
kauan <kauansbflu@gmail.com>
2025-11-03 12:06:26 -0300

    feat: implement user authentication with login view and template

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 
     path('', views.home, name='home'),
     path('', include('brands.urls')),
