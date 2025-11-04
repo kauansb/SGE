cfd9a93dc9be6ccbca645f53e3031ea26c711b8d
kauan <kauansbflu@gmail.com>
2025-11-04 12:36:57 -0300

    feat: add DjangoModelPermissions to REST framework default permission classes

--- app/settings.py
+++ app/settings.py
@@ -133,6 +133,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.DjangoModelPermissions',
     ),
 }
 
